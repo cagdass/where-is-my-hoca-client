6a825205c6ad86fe8bcc3b04a955354cf67f8bc5
Çağdaş <cagdas.oztekin@ug.bilkent.edu.tr>
2016-08-11 13:49:23 +0300

    Remove test Grids.

--- src/components/_components/about.js
+++ src/components/_components/about.js
@@ -16,17 +16,6 @@ class About extends React.Component {
                 <br/>
                 <p>Learning <i>some</i> React made it possible. So thanks go to <a href="http://20satir.com">20satir</a> where I got to learn what I learned and also Facebook, who made React in the first place.</p>
             </Row>
-            <div>
-                <Grid>
-                    <Row className="show-grid">
-                        <Col sm={6} md={3}><code>&lt;{'Col sm={6} md={3}'} /&gt;</code><br/>{"aasud as diua sdhi asd jajksd kakjsdkasdkj ajksdk jkas djk kjas dkj jkas dkj jakhsdjkhakjsd kjahksdhihaisdhihaisud iashd jashdj asdamsd am sdjahsd"}</Col>
-                        <Col sm={6} md={3}><code>&lt;{'Col sm={6} md={3}'} /&gt;</code><br/>{"asd jajksd kakjsdkasdkj ajksdk jkas djk kjas dkj jkas dkj jakhsdjkhakjsd kjahksdhihaisdhihaisud iashd jashdj a"}</Col>
-                        <Clearfix visibleSmBlock><code>&lt;{'Clearfix visibleSmBlock'} /&gt;</code></Clearfix>
-                        <Col sm={6} md={3}><code>&lt;{'Col sm={6} md={3}'} /&gt;</code><br/>{" asd jajksd kakjsdkasdkj ajksdk jkas djk kjas dkj jkas dkj jakhsdjkhakjsd kjahksdhihaisdhihaisud iashd jashdj a  asd jajksd kakjsdkasdkj ajksdk jkas djk kjas dkj jkas dkj jakhsdjkhakjsd kjahksdhihaisdhihaisud iashd jashdj a"}</Col>
-                        <Col sm={6} md={3}><code>&lt;{'Col sm={6} md={3}'} /&gt;</code><br/>{"jkas djk kjas dkj jkas dkj jakhsdjkhakjsd kjahksdhihaisdhihaisud iashd jashdj a  asd jajksd kakjsdkasdkj ajksdk jkas djk kjas dkj"}</Col>
-                    </Row>
-                </Grid>
-            </div>
         </div>)
     }
 }
