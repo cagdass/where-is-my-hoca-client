a8d8c8ee6e25fda0a2ab0ec62e854381851d1980
Çağdaş <cagdas.oztekin@ug.bilkent.edu.tr>
2016-08-16 22:13:43 +0300

    Add selector for buildings.

--- src/components/classroom/empty_classrooms.js
+++ src/components/classroom/empty_classrooms.js
@@ -14,17 +14,36 @@ class EmptyClassrooms extends React.Component {
         super(props, context, ...args);
 
         this.state = {
-            clicked: []
+            clicked: [],
+            buildings: [],
+            loaded: false,
+            selectedBuilding: "All"
         };
     }
 
-    // componentWillMount() {
-    //     let {searchParams, orderParams, pager} = this.state;
-    //     this.searchClassrooms(searchParams, orderParams, pager).then(() => {
-    //         this.setState({loaded: true});
-    //         this.sortClassrooms();
-    //     });
-    // }
+    componentWillMount() {
+        let {searchParams, orderParams, pager} = this.state;
+        this.searchBuildings(searchParams, orderParams, pager).then(() => {
+            this.setState({loaded: true});
+            this.sortBuildings();
+        });
+    }
+
+    searchBuildings() {
+        return scheduleService.findDistinctBuildings()
+        .then(buildings => {
+            this.setState({buildings})
+        })
+        .catch(searchError => this.setState({searchError}));
+    }
+
+    sortBuildings(){
+        let {buildings = []} = this.state;
+        this.setState({
+            "buildings": buildings.sort(),
+            "loaded": true
+        });
+    }
 
     // // Credits go to react-auto-suggest, which is no longer a dependency in this project :(.
     // escapeRegexCharacters(str) {
@@ -32,14 +51,17 @@ class EmptyClassrooms extends React.Component {
     // }
 
     getHour(num) {
-        if(num < 2) {
-            return "0" + (num + 8) + ":40";
+        if(num < 1) {
+            return "0" + (num + 8) + ":40-0" + (num+9) + ":30";
+        }
+        else if(num < 2){
+            return "0" + (num + 8) + ":40-" + (num+9) + ":30";
         }
         else if(num >= 2 && num < 4) {
-            return (num + 8) + ":40";
+            return (num + 8) + ":40-" + (num + 9) + ":30";
         }
         else {
-            return (num + 9) + ":40";
+            return (num + 9) + ":40-" + (num + 10) + ":30";
         }
     }
 
@@ -53,37 +75,79 @@ class EmptyClassrooms extends React.Component {
             clicked.push(index);
         }
 
+        console.log(clicked);
         this.setState({clicked})
+
     }
 
     renderRow(xs, md, index) {
-        return <Row className="rowasd show-grid">
+        return <Row className="sg-row show-grid">
             {[0, 1, 2, 3, 4].map(i => i + index * 5).map(this.renderButton.bind(this, xs, md))}
         </Row>
     }
 
     renderButton(xs, md, index) {
         let {clicked} = this.state;
-        let style = "bg-colWhite";
+        var style = "";
 
         if(clicked.indexOf(index) != -1) {
-            style = "bg-colSel"
+            style = "sg-col-selected"
         }
 
-        return <Col xs={xs} md={md} className={`bg-col ${style}`} key={index} onClick={this.handleClick.bind(this, index)}>
+        return <Col xs={xs} md={md} className={`sg-col ${style}`} key={index} onClick={this.handleClick.bind(this, index)}>
             {this.getHour(Math.floor(index / 5))}
         </Col>
     }
 
+    handleBuildingChange(event) {
+        let selectedBuilding = event.target.value;
+        this.setState({selectedBuilding});
+    }
+
+    renderBuilding(building) {
+        return <option value={building}>{building}</option>;
+    }
 
     render() {
+        let [xs, md] = [2, 2];
+        let {buildings, loaded} = this.state;
+
         return <div>
             <UnderConstrution />
-            <Grid>
-                {[0, 1, 2, 3].map(this.renderRow.bind(this, 2, 2))}
-                <hr />
-                {[4, 5, 6, 7].map(this.renderRow.bind(this, 2, 2))}
-            </Grid>
+            <Loader loaded={loaded}>
+                <Row>
+                    <Col xs={4} md={4}>
+                        <FormGroup controlId="formControlsSelect">
+                            <Row>
+                                <Col>
+                                    <ControlLabel>Select a Building</ControlLabel>
+                                </Col>
+                                <Col>
+                                    <FormControl onChange={this.handleBuildingChange.bind(this)} componentClass="select" placeholder="select">
+                                        <option value="All">All Buildings</option>
+                                        {buildings.map(this.renderBuilding.bind(this))}
+                                    </FormControl>
+                                </Col>
+                            </Row>
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Grid>
+                    <Col>
+                        <Row className="show-grid">
+                            <Col className="sg-col" xs={xs} md={md}>Monday</Col>
+                            <Col className="sg-col" xs={xs} md={md}>Tuesday</Col>
+                            <Col className="sg-col" xs={xs} md={md}>Wednesday</Col>
+                            <Col className="sg-col" xs={xs} md={md}>Thursday</Col>
+                            <Col className="sg-col" xs={xs} md={md}>Friday</Col>
+                        </Row>
+
+                        {[0, 1, 2, 3].map(this.renderRow.bind(this, 2, 2))}
+                        <hr />
+                        {[4, 5, 6, 7].map(this.renderRow.bind(this, 2, 2))}
+                    </Col>
+                </Grid>
+            </Loader>
         </div>;
     }
 }
