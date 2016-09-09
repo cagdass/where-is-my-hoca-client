420e70b7e7ce953e477c55e4e0925e3bda972dcc
Çağdaş <cagdas.oztekin@ug.bilkent.edu.tr>
2016-09-09 07:06:14 +0300

    F*** browser history.

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import Promise from "bluebird";
 import moment_locale_tr from "moment/locale/tr.js";
 import React from "react";
 import ReactDOM, { render } from "react-dom";
-import { Router, browserHistory } from "react-router";
+import { Router, hashHistory } from "react-router";
 
 import "whatwg-fetch";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -14,4 +14,4 @@ import routes from "./routes.js";
 moment.updateLocale("tr", moment_locale_tr);
 Promise.config({longStackTraces: true, warnings: true});
 
-render(<Router routes={routes} history={browserHistory}/>, document.getElementById("content"));
+render(<Router routes={routes} history={hashHistory}/>, document.getElementById("content"));
