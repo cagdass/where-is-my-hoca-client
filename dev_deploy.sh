34e640c8b16de950cd67f4eb078d05b2f5aa8965
Çağdaş <cagdas.oztekin@ug.bilkent.edu.tr>
2016-09-28 17:10:17 +0300

    Update gitignore

--- dev_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf dev_deploy
-mkdir dev_deploy
-cp -R deploy dev_deploy
-rm dev_deploy.tar.gz
-tar cvzf dev_deploy.tar.gz dev_deploy/
-scp dev_deploy.tar.gz root@cgds.me:/usr/share/nginx/html/
